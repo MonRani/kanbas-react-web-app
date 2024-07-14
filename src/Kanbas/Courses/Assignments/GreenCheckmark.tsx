213a8cf1e133fc761c2c014c66bc91343a368853
Monisha Rani <monisharani@Monishas-Air.attlocal.net>
2024-07-14 14:25:08 -0700

    a2 CSS and Bootstrap

---
+++ src/Kanbas/Courses/Assignments/GreenCheckmark.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FaCheckCircle, FaCircle } from 'react-icons/fa';
+
+export default function GreenCheckmark() {
+  return (
+    <span className="me-1 position-relative">
+      <FaCheckCircle style={{ top: "5px" }} className="text-success me-1 position-absolute fs-5" />
+      <FaCircle className="text-white fs-6" />
+    </span>
+  );
+}
