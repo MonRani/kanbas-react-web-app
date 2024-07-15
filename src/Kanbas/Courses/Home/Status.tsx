69bda5f04d464a28dbf5ad43e0c040017f7ac4ae
MonRani <107244250+MonRani@users.noreply.github.com>
2024-07-15 16:18:32 -0700

    Update Status.tsx

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -9,50 +9,49 @@ import { TfiAnnouncement } from "react-icons/tfi";
 
 export default function CourseStatus() {
   return (
-    <div id="wd-course-status" style={{ width: "300px" }}>
+    <div id="wd-course-status" className="d-none d-xl-block p-3" style={{ width: "300px", marginLeft: '50px', zIndex: 100 }}>
       <h2>Course Status</h2>
       <div className="d-flex">
         <div className="w-50 pe-1">
-          <button className="btn btn-lg btn-secondary w-100 text-nowrap">
-            <MdDoNotDisturbAlt className="me-2 fs-5" />
+          <button className="btn btn-secondary w-100 text-start">
+            <MdDoNotDisturbAlt className="me-2 fs-4" />
             Unpublish
           </button>
         </div>
         <div className="w-50">
-          <button className="btn btn-lg btn-success w-100">
-            <FaCheckCircle className="me-2 fs-5" />
+          <button className="btn btn-success w-100 text-start">
+            <FaCheckCircle className="me-2 fs-4" />
             Publish
           </button>
         </div>
       </div>
       <br />
-      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <BiImport className="me-2 fs-5" />
+      <button className="btn btn-secondary w-100 mt-1 text-start">
+        <BiImport className="me-2 fs-4" />
         Import Existing Content
       </button>
-      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <LiaFileImportSolid className="me-2 fs-5" />
+      <button className="btn btn-secondary w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-4" />
         Import from Commons
       </button>
-      {/* Additional buttons */}
-      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <IoMdHome className="me-2 fs-5" />
+      <button className="btn btn-secondary w-100 mt-1 text-start">
+        <IoMdHome className="me-2 fs-4" />
         Choose Home Page
       </button>
-      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <BsFillFileBarGraphFill className="me-2 fs-5" />
+      <button className="btn btn-secondary w-100 mt-1 text-start">
+        <BsFillFileBarGraphFill className="me-2 fs-4" />
         View Course Screen
       </button>
-      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <TfiAnnouncement className="me-2 fs-5" />
+      <button className="btn btn-secondary w-100 mt-1 text-start">
+        <TfiAnnouncement className="me-2 fs-4" />
         New Announcement
       </button>
-      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <BsFillFileBarGraphFill className="me-2 fs-5" />
+      <button className="btn btn-secondary w-100 mt-1 text-start">
+        <BsFillFileBarGraphFill className="me-2 fs-4" />
         New Analytics
       </button>
-      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <FaBell className="me-2 fs-5" />
+      <button className="btn btn-secondary w-100 mt-1 text-start">
+        <FaBell className="me-2 fs-4" />
         View Course Notifications
       </button>
     </div>
